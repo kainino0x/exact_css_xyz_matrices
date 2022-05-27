5664e5d4f50196d8fcd9ce46db42ffe7c416e08f
Kai Ninomiya <kainino@chromium.org>
2022-05-26 18:03:44 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,46 @@ fn chromaticity(x: (i128, i128), y: (i128, i128)) -> Chromaticity {
 fn print_vector(header: &str, vector: &[Scalar; 3]) {
     print!("{}: [", header);
     for (idx, value) in vector.iter().enumerate() {
-        print!("{} {} / {}",
-               if idx == 0 { "" } else { "," },
-               value.numer(), value.denom());
+        print!(
+            "{} {} / {}",
+            if idx == 0 { "" } else { "," },
+            value.numer(),
+            value.denom()
+        );
     }
     println!(" ]");
 }
 
 fn print_matrix(header: &str, matrix: &[[Scalar; 3]; 3]) {
-    fn make_array<T>(f: impl Fn(usize) -> T) -> [T; 3] { [f(0), f(1), f(2)] }
+    fn make_array<T>(f: impl Fn(usize) -> T) -> [T; 3] {
+        [f(0), f(1), f(2)]
+    }
 
-    let formatted = make_array(|row| make_array(|col| {
-        let value = &matrix[row][col];
-        (format!("{}", value.numer()), format!("{}", value.denom()))
-    }));
-    let lengths = make_array(|col| (
-        formatted.iter().map(|row| row[col].0.len()).max().unwrap(),
-        formatted.iter().map(|row| row[col].1.len()).max().unwrap(),
-    ));
+    let formatted = make_array(|row| {
+        make_array(|col| {
+            let value = &matrix[row][col];
+            (format!("{}", value.numer()), format!("{}", value.denom()))
+        })
+    });
+    let lengths = make_array(|col| {
+        (
+            formatted.iter().map(|row| row[col].0.len()).max().unwrap(),
+            formatted.iter().map(|row| row[col].1.len()).max().unwrap(),
+        )
+    });
 
     println!("{}:", header);
     for row in formatted.iter() {
         print!("  [");
         for (idx, value) in row.iter().enumerate() {
-            print!("{comma}{numer:>numer_len$} / {denom:>denom_len$}",
-                   comma = if idx == 0 { "" } else { ", " },
-                   numer = value.0, numer_len = lengths[idx].0,
-                   denom = value.1, denom_len = lengths[idx].1);
+            print!(
+                "{comma}{numer:>numer_len$} / {denom:>denom_len$}",
+                comma = if idx == 0 { "" } else { ", " },
+                numer = value.0,
+                numer_len = lengths[idx].0,
+                denom = value.1,
+                denom_len = lengths[idx].1
+            );
         }
         println!("],");
     }
@@ -56,8 +69,7 @@ fn main() {
             chromaticity((15, 100), (6, 100)),
         ];
 
-        let matrix = rgb_derivation::matrix::calculate(
-            &d65_white_xyz, &primaries_xy).unwrap();
+        let matrix = rgb_derivation::matrix::calculate(&d65_white_xyz, &primaries_xy).unwrap();
         let inverse = rgb_derivation::matrix::inversed_copy(&matrix).unwrap();
         let primaries_xyz = rgb_derivation::matrix::transposed_copy(&matrix);
 
@@ -73,8 +85,7 @@ fn main() {
             chromaticity((15, 100), (6, 100)),
         ];
 
-        let matrix = rgb_derivation::matrix::calculate(
-            &d65_white_xyz, &primaries_xy).unwrap();
+        let matrix = rgb_derivation::matrix::calculate(&d65_white_xyz, &primaries_xy).unwrap();
         let inverse = rgb_derivation::matrix::inversed_copy(&matrix).unwrap();
         let primaries_xyz = rgb_derivation::matrix::transposed_copy(&matrix);
 
